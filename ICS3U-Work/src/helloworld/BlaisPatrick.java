39e408e257e4f5aeffda594a4fa53070307199da
Patrick <pwblais@icloud.com>
2016-09-30 14:34:38 -0400

    First comit

---
+++ ICS3U-Work/src/helloworld/BlaisPatrick.java
@@ -0,0 +1,32 @@
+package helloworld;
+
+public class BlaisPatrick {
+	
+	public static void main(String[] args) {
+/**
+ * 	PPPPPPPPPPPP              A                  TTTTTTTTTTTTT     
+ * P                   P           A A                          T
+ * P                   P          A   A                         T
+ * P                   P         A     A                        T
+ * P                   P        A       A                       T
+ * PPPPPPPPPPPP         A         A                      T
+ * P                           AAAAAAA                      T
+ * P                          A             A                    T
+ * P                         A               A                   T
+ * P                        A                 A                  T
+ * P                       A                   A                 T
+ * 
+ */
+		
+		System.out.println("PPPPPPPPPPPP              A                  TTTTTTTTTTTTT");
+		System.out.println("P                   P           A A                          T");
+		System.out.println("P                   P          A   A                         T");
+		System.out.println("P                   P         A     A                        T");
+		System.out.println("PPPPPPPPPPPP         A         A                      T");
+		System.out.println("P                           AAAAAAA                      T");
+		System.out.println("P                          A             A                    T");
+		System.out.println("P                         A               A                   T");
+		System.out.println("P                        A                 A                  T");
+		System.out.println("P                       A                   A                 T");
+	}
+}
